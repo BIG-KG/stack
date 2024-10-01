0eef448e534cbea6cb7e23df8e692471e6c5cb86
BIG-KG <bek.georgy@gmail.com>
2024-10-01 04:04:31 +0300

    cbnh

--- main.cpp
+++ main.cpp
@@ -16,8 +16,25 @@ int main()
     push(test, 6);
     push(test, 7);
     printf("%d\n", pop(test));
-    printf("%d\n", pop(test));
-    printf("%d\n", pop(test));
+
+    int test2 = stack_ctor();
+
+    push(test2, 0);
+    push(test2, 1);
+    push(test2, 2);
+    printf("%d\n", pop(test2));
+    printf("%d\n", pop(test2));
 
 
+    printf("a  = %d\n", pop(test));
+
+    stack_dtor(test2,  1);
+    printf("     wer         \n");
+    stack_dtor(test, 1);
+    printf("tyu");
+
+
+
+
+    return 0;
 }
